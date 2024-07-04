8b96dc6fa6579ff261344405cd0c5122735b323a
korsmakolnikov <korsmakolnikov@gmail.com>
2024-07-04 23:15:20 +0200

    Disable lualine due to its instability

--- lua/visual/lualine.lua
+++ lua/visual/lualine.lua
@@ -7,13 +7,13 @@ end
 
 local shorten_branch_name = shorten_by(10)
 
-require('lualine').setup({
-  options = {
-    globalstatus = true
-  },
-  sections = {
-    lualine_b = { { 'branch', fmt = shorten_branch_name }, 'diff', 'diagnostics' },
-    lualine_c = { { 'filename', path = 1 }, "require'lsp-status'.status()", "require'lsp-status'.register_progress()" },
-    lualine_x = { 'encoding', 'filetype', "os.date('%H:%M')" },
-  }
-})
+-- require('lualine').setup({
+--   options = {
+--     globalstatus = true
+--   },
+--   sections = {
+--     lualine_b = { { 'branch', fmt = shorten_branch_name }, 'diff', 'diagnostics' },
+--     lualine_c = { { 'filename', path = 1 }, "require'lsp-status'.status()", "require'lsp-status'.register_progress()" },
+--     lualine_x = { 'encoding', 'filetype', "os.date('%H:%M')" },
+--   }
+-- })
