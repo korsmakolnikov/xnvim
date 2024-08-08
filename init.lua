88951c44ecd2092afcf7609c1daf08b6a49f178f
Your Name <you@example.com>
2024-08-08 23:09:25 +0200

    Wip

--- init.lua
+++ init.lua
@@ -1,27 +1,27 @@
 local pack = "tangerine" or "packer" or "paq" or "lazy"
 
 local function bootstrap(url, ref)
-    local name = url:gsub(".*/", "")
-    local path
+  local name = url:gsub(".*/", "")
+  local path
 
-    if pack == "lazy" then
-        path = vim.fn.stdpath("data") .. "/lazy/" .. name
-        vim.opt.rtp:prepend(path)
-    else
-        path = vim.fn.stdpath("data") .. "/site/pack/".. pack .. "/start/" .. name
+  if pack == "lazy" then
+    path = vim.fn.stdpath("data") .. "/lazy/" .. name
+    vim.opt.rtp:prepend(path)
+  else
+    path = vim.fn.stdpath("data") .. "/site/pack/" .. pack .. "/start/" .. name
+  end
+
+  if vim.fn.isdirectory(path) == 0 then
+    print(name .. ": installing in data dir...")
+
+    vim.fn.system { "git", "clone", url, path }
+    if ref then
+      vim.fn.system { "git", "-C", path, "checkout", ref }
     end
 
-    if vim.fn.isdirectory(path) == 0 then
-        print(name .. ": installing in data dir...")
-
-        vim.fn.system {"git", "clone", url, path}
-        if ref then
-            vim.fn.system {"git", "-C", path, "checkout", ref}
-        end
-
-        vim.cmd "redraw"
-        print(name .. ": finished installing")
-    end
+    vim.cmd "redraw"
+    print(name .. ": finished installing")
+  end
 end
 
 -- for git head
