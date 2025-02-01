1a8b97e7449854cee9ab3487194f835b9687f862
Korsmakolnikov <korsmakolnikov@gmail.com>
2025-02-01 19:40:12 +0100

    Add trouble plugin and some minor binding fix

--- lua/packages.lua
+++ lua/packages.lua
@@ -224,5 +224,10 @@ return lazy.setup(
           require('tabby').setup()
         end,
       },
+      {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+      }
     }
   })
