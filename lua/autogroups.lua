cd89120b71c08614ec372741f50b7a4619751fe8
korsmakolnikov <korsmakolnikov@gmail.com>
2026-03-13 17:17:38 +0100

    feat: add autogroups to start/stop ollama

--- lua/autogroups.lua
+++ lua/autogroups.lua
@@ -7,4 +7,16 @@ vim.api.nvim_create_autocmd("BufWritePre", { callback = _1_, group = format_sync
 vim.api.nvim_create_autocmd("BufEnter",
   { command = ":set autoindent noexpandtab tabstop=4 shiftwidth=4", pattern = { "*.rs", "*.s", "*.asm", "*.php" } })
 vim.api.nvim_create_autocmd("BufEnter",
-  { command = ":set autoindent noexpandtab tabstop=8 shiftwidth=8", pattern = { "*.c", "*.h", "*.cpp", "*.hpp", "*.go"} })
+  { command = ":set autoindent noexpandtab tabstop=8 shiftwidth=8", pattern = { "*.c", "*.h", "*.cpp", "*.hpp", "*.go" } })
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function()
+    vim.system({ "ollama", "serve" }, { detach = true })
+  end
+})
+
+vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
+  callback = function()
+    vim.system({ "ollama", "stop", "wizardcoder" })
+  end
+})
