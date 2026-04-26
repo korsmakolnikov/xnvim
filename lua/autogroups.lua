2ce4d574d51723230d9c33df714ec1b619c0f81c
Federico Caprari <korsmakolnikov@gmail.com>
2026-04-26 17:22:18 +0200

    fix: python indentation

--- lua/autogroups.lua
+++ lua/autogroups.lua
@@ -7,4 +7,4 @@ vim.api.nvim_create_autocmd("BufWritePre", { callback = _1_, group = format_sync
 vim.api.nvim_create_autocmd("BufEnter",
   { command = ":set autoindent noexpandtab tabstop=4 shiftwidth=4", pattern = { "*.rs", "*.s", "*.asm", "*.php" } })
 vim.api.nvim_create_autocmd("BufEnter",
-  { command = ":set autoindent noexpandtab tabstop=8 shiftwidth=8", pattern = { "*.c", "*.h", "*.cpp", "*.hpp", "*.go"} })
+  { command = ":set autoindent noexpandtab tabstop=8 shiftwidth=8", pattern = { "*.c", "*.h", "*.cpp", "*.hpp", "*.go", "*.py" } })
